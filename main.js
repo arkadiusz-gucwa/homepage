a9beeea9f0a9f36f9e02281bb31b6a1623c47ae1
Arkadiusz Gucwa <agie@onet.com.pl>
2020-05-21 15:52:51 +0200

    js-greet

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ console.log(`Nazywam się ${name} i mam ${age} lata`)
 
 const emptyParagrapf = document.querySelector('.week-summary__description--js');
 emptyParagrapf.innerHTML='Nawet uzupelnilem tresc JavaScriptem';
+
+const greet = (age, firstName) => {
+    console.log(
+        `Witaj Drogi odwiedzający nazywam sie ${firstName} i mam ${age} lat.`
+
+    );
+}
+
+greet(31, `Arek`)
